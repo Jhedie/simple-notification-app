39b9c4a5fbe82841788ae41e5dfd24501887a404
Jhedie <jeddiahawuku12@gmail.com>
2022-12-08 04:16:15 +0000

    feat: Add notification count to App component
    Change app component html and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'simple-notification-app';
+  title = 'Simple Notification App';
+
+  notificationCount = 0;
 }
